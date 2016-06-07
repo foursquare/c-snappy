06f3a271875696bf6a20ecd60b3d96c0d1fdc6b8
Billy Seol <seol@foursquare.com>
2016-06-07 19:43:12 +0000

    cgo flag change for quiver

--- cgo_flags.go
+++ cgo_flags.go
@@ -2,7 +2,7 @@
 // Snappy C++ library.
 package snappy
 
-// #cgo CXXFLAGS: -std=c++11
+// #cgo CXXFLAGS: -std=c++0x
 // #cgo CPPFLAGS: -DHAVE_CONFIG_H -Iinternal
 // #cgo !windows CPPFLAGS: -DHAVE_SYS_MMAN_H
 // #cgo windows CPPFLAGS: -DHAVE_WINDOWS_H
